bc2c0f92b7e74cf1bd6f15d2e30c87c2c32035bc
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-12-09 12:50:45 -0600

    getting ready for deployment and commented out the live link to santas helper for now

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -11,7 +11,7 @@ const Projects = () => {
 			description: "A full stack application designed for users to create Christmas Wish Lists for their loved ones and keep track of whether the items have been bought or wrapped. Users can also send secret messages to Santa at the North Pole",
 			meta: 'MongoDB, Express, React.js, Node.js, and Bootstrap-React, Sass, and an optional alternative back end with Django and Python',
 			image: "SantasHelper.png",
-			href: 'https://emilybernard1.github.io/santas-helper/',
+			// href: 'https://emilybernard1.github.io/santas-helper/',
 			signIn: 'Username: eb@eb.eb Password: eb',
 			gitHub: 'https://github.com/emilybernard1/santas-helper'
 		},
@@ -61,7 +61,7 @@ const Projects = () => {
 				<a
 					href={project.href}
 					target='_blank'
-					rel="noreferrer"
+					rel="noopener"
 					aria-label={project.header}
 
 				><Button basic color='purple'>Check Out the Live Site</Button></a>
