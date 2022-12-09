97e97918a76ff9ccbcdc38ff7508e1c61ddb78a1
Emily (Bare) Bernard <77028684+ebare012882@users.noreply.github.com>
2022-12-09 09:17:29 -0600

    styled the contact me section more by putting it in a card and centering the header then changed the background color to black

--- src/components/EmailMe.js
+++ src/components/EmailMe.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import emailjs from '@emailjs/browser';
-import { Segment, Header } from 'semantic-ui-react'
+import { Segment, Header, Card } from 'semantic-ui-react'
 import { useState } from "react";
 
 const EmailMe = (props) => {
@@ -23,41 +23,43 @@ const EmailMe = (props) => {
 
 
     return (
-        <>
+
+        <div className='contactme'>
             <div id="top0">
                 <div id="top1">
                     <Segment fluid style={{ padding: '1em 0em' }} vertical color='purple'>
                     </Segment>
-                    <Header as='h1' style={{ fontSize: '2em', color: 'violet' }}>
+                    <Header as='h1' style={{ fontSize: '2em', color: 'violet' }} textAlign='center' >
                         Contact Me
+                        <br></br>
                     </Header>
-
-                    <p style={{ fontSize: '1.33em' }}>I would love to hear from you! Please leave me a message! </p>
+                    <p style={{ fontSize: '1.33em', textAlign: 'center', color: 'aqua' }} >I would love to hear from you! Please leave me a message! </p>
                 </div>
             </div>
             <br></br>
-            <div id="top2" style={{ fontSize: '1.33em' }}>
-                <form id='contact-form' onSubmit={sendEmail}>
-                    <label class='contact-label'> Your Name: </label>
-                    <input class='text-input' type="text" name="from_name" placeholder="your name..." ></input>
-                    <br></br>
-                    <br></br>
-                    <label class='contact-label'> Your Email Address: </label>
-                    <input class='text-input' type="text" name="reply_to" placeholder="your email address..." ></input>
-                    <br></br>
-                    <br></br>
-                    <label class='contact-label'> Message: </label>
-                    <textarea class='text-input' name="message" placeholder="your message here...">
-                    </textarea>
-                    <br></br>
-                    <br></br>
-                    <input id='contact-submit' type="submit" value="Submit" margin='3em'></input>
-                    <br></br>
-                    <br></br>
-                </form>
-
-            </div>
-        </>
+            <Card fluid style={{ padding: '2em', backgroundColor: 'black'}}>
+                <div id="top2" style={{ fontSize: '1.33em', color: 'white', justifyContent: 'center', alignItems: 'center', alignContent: 'center' }}>
+                    <form id='contact-form' onSubmit={sendEmail}>
+                        <label class='contact-label'> Your Name: </label>
+                        <input class='text-input' type="text" name="from_name" placeholder="your name..." ></input>
+                        <br></br>
+                        <br></br>
+                        <label class='contact-label'> Your Email Address: </label>
+                        <input class='text-input' type="text" name="reply_to" placeholder="your email address..." ></input>
+                        <br></br>
+                        <br></br>
+                        <label class='contact-label'> Message: </label>
+                        <textarea class='text-input' name="message" placeholder="your message here...">
+                        </textarea>
+                        <br></br>
+                        <br></br>
+                        <input id='contact-submit' type="submit" value="Submit" margin='3em'></input>
+                        <br></br>
+                        <br></br>
+                    </form>
+                </div>
+            </Card>
+        </div>
 
 
     )
